96601568729679044e65a993f54cd66f223c5d6f
marwandev <marwaneg@aucegypt.edu>
2025-03-31 13:32:18 +0200

    Add the issue licence option data, business and cms

---
+++ DLMS_Business/Driver.cs
@@ -0,0 +1,27 @@
+﻿using DLMS_DataAccess.Person;
+using System;
+
+namespace DLMS_Business
+{
+    public class Driver
+    {
+        public int ID { get; set; }
+        public int PersonId { get; set; }
+        public int CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Driver()
+        {
+            this.ID = -1;
+            this.PersonId = -1;
+            this.CreatedByUserId = -1;
+            this.CreatedDate = new DateTime();
+        }
+
+        public bool AddNewDriver()
+        {
+            this.ID = DriverData.AddNewDriver(PersonId, CreatedByUserId, CreatedDate);
+            return this.ID != -1;
+        }
+    }
+}
---
+++ DLMS_Business/Licence.cs
@@ -0,0 +1,42 @@
+﻿using DLMS_DataAccess.Licence;
+using System;
+
+namespace DLMS_Business
+{
+    public class Licence
+    {
+        public int ID { get; set; }
+        public int ApplicationId { get; set; }
+        public int DriverId { get; set; }
+        public int LicenceClassId { get; set; }
+        public int CreatedByUserId { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public bool IsActive { get; set; }
+        public string Notes { get; set; }
+        public decimal PaidFees { get; set; }
+        public byte IssueReason { get; set; }
+
+        public Licence()
+        {
+            this.ID = -1;
+            this.ApplicationId = -1;
+            this.LicenceClassId = -1;
+            this.CreatedByUserId = -1;
+            this.DriverId = -1;
+            this.IssueDate = new DateTime();
+            this.ExpirationDate = new DateTime();
+            this.IsActive = false;
+            this.Notes = "";
+            this.PaidFees = 0;
+            this.IssueReason = 0;
+        }
+
+        public bool AddNewLicence()
+        {
+            this.ID = LicenceData.AddNewLicence(ApplicationId, DriverId, LicenceClassId, IssueDate, IsActive,
+                Notes, ExpirationDate, PaidFees, CreatedByUserId, IssueReason);
+            return this.ID != -1;
+        }
+    }
+}
