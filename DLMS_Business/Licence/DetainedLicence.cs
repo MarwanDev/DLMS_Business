8cefb7eaa5b2c51932c83e863934b2f0d987697f
marwandev <marwaneg@aucegypt.edu>
2025-04-04 22:39:15 +0200

    Add the licence release feature

--- DLMS_Business/Licence/DetainedLicence.cs
+++ DLMS_Business/Licence/DetainedLicence.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace DLMS_Business.Licence
         public int ID { get; set; }
         public int LicenceID { get; set; }
         public DateTime DetainDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
         public decimal FineFees { get; set; }
         public int CreatedByUserId { get; set; }
         public bool IsReleased { get; set; }
@@ -30,9 +32,54 @@ namespace DLMS_Business.Licence
             this.IsReleased = false;
         }
 
+        private DetainedLicence(int id, int licenceId, DateTime detainDate, decimal fineFees,
+            int createdByUserId, bool isReleased, DateTime releaseDate, int releasedByUserId, int releaseApplicationId)
+        {
+            this.ID = id;
+            this.LicenceID = licenceId;
+            this.CreatedByUserId = createdByUserId;
+            this.DetainDate = detainDate;
+            this.FineFees = fineFees;
+            this.IsReleased = isReleased;
+            this.ReleaseDate = releaseDate;
+            this.ReleaseApplicationId = releaseApplicationId;
+            this.ReleasedByUserId = releasedByUserId;
+        }
+
         public static bool IsLicenceDetained(int licenceId)
         {
             return DetainedLicenceData.IsLicenceDetained(licenceId);
+        }
+
+        public static DetainedLicence Find(int id)
+        {
+            int licenceId = -1, createdByUserId = -1, releaseApplicationId = -1, releasedByUserId = -1;
+            DateTime detainDate = new DateTime(), releaseDate = new DateTime();
+            decimal fineFees = 0;
+            bool isReleased = false;
+            if (DetainedLicenceData.GetDetainedLicenceDatById(id, ref licenceId, ref detainDate, ref fineFees,
+                ref createdByUserId, ref isReleased, ref releaseDate, ref releasedByUserId, ref releaseApplicationId))
+                return new DetainedLicence(id, licenceId, detainDate, fineFees,
+                createdByUserId, isReleased, releaseDate, releasedByUserId, releaseApplicationId);
+            else return null;
+        }
+
+        public static DetainedLicence FindbyLicenceId(int licenceId)
+        {
+            int id = -1, createdByUserId = -1, releaseApplicationId = -1, releasedByUserId = -1;
+            DateTime detainDate = new DateTime(), releaseDate = new DateTime();
+            decimal fineFees = 0;
+            bool isReleased = false;
+            if (DetainedLicenceData.GetDetainedLicenceDatByLicenceId(ref id, licenceId, ref detainDate, ref fineFees,
+                ref createdByUserId, ref isReleased, ref releaseDate, ref releasedByUserId, ref releaseApplicationId))
+                return new DetainedLicence(id, licenceId, detainDate, fineFees,
+                createdByUserId, isReleased, releaseDate, releasedByUserId, releaseApplicationId);
+            else return null;
+        }
+
+        public bool ReleaseDetainedLicence()
+        {
+            return DetainedLicenceData.ReleaseDetainedLicence(ID, ReleaseDate, ReleasedByUserId, ReleaseApplicationId);
         }
     }
 }
